f63c7004fbacfda9366c986bb9ae6e70ca677f41
Spencer Cousino <skc29@pitt.edu>
2017-02-25 14:20:43 -0500

    Initial commit

---
+++ app/src/main/java/edu/pitt/cs/cs1635/skc29/mobilevoting/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.pitt.cs.cs1635.skc29.mobilevoting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
